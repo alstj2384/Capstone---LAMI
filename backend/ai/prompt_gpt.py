1af7f92da334b8e61f43f75e904a7d90a5aab4a9
chulyoung <ccy09671324@gmail.com>
2025-05-26 19:30:29 +0900

    change gpt model, fix feedback, memorization response

--- backend/ai/prompt_gpt.py
+++ backend/ai/prompt_gpt.py
@@ -4,12 +4,16 @@ import json
 # env 파일 불러오기
 from dotenv import load_dotenv
 from fastapi import HTTPException
+# 응답 형식 불러오기
+from prompt_response_model import RESPONSE_WORKBOOK_FORMAT
+from prompt_response_model import RESPONSE_FEEDBACK_FORMAT
+from prompt_response_model import RESPONSE_MEMORIZATION_FORMAT
 
 load_dotenv()
 
 GPT_API_KEY = os.getenv("GPT_API_KEY")
 DIR_PATH = os.getenv("DIR_PATH")
-MODEL = "gpt-4-1106-preview"
+MODEL = "gpt-3.5-turbo-0125"
 client = OpenAI(
     api_key=GPT_API_KEY
 )
@@ -17,13 +21,13 @@ client = OpenAI(
 
 # GPT에게 문제집 생성
 def post_gpt_make_workbook(pdf, difficulty, multiple_choice_amount, true_false_amount, short_answer_amount):
-    request_msg = "한국어로"
-    request_msg += "해당 pdf에 내용을 바탕으로"
-    request_msg += "난이도는" + ("하" if difficulty == 1 else "중" if difficulty == 2 else "상" if difficulty == 3 else "생략")
-    request_msg += "O/X 문제" + str(multiple_choice_amount) + "개"
-    request_msg += "객관식 문제" + str(true_false_amount) + "개"
-    request_msg += "단답형 문제" + str(short_answer_amount) + "개"
-    request_msg += "문제 전체 형식은 다음과 같다." + RESPONSE_WORKBOOK_FORMAT
+    request_msg = "한국어로 " + "\n"
+    request_msg += "해당 pdf에 내용을 바탕으로 " + "\n"
+    request_msg += "난이도는 " + ("하" if difficulty == 1 else "중 " if difficulty == 2 else "상 " if difficulty == 3 else "생략 ") + "\n"
+    request_msg += "O/X 문제 " + str(multiple_choice_amount) + "개 " + "\n"
+    request_msg += "객관식 문제 " + str(true_false_amount) + "개 " + "\n"
+    request_msg += "단답형 문제 " + str(short_answer_amount) + "개 " + "\n"
+    request_msg += "내가 원하는 응답 형식: " + RESPONSE_WORKBOOK_FORMAT
 
     try:
         file = client.files.create(
@@ -33,7 +37,6 @@ def post_gpt_make_workbook(pdf, difficulty, multiple_choice_amount, true_false_a
     except Exception as e:
         raise HTTPException(status_code=400, detail="No file error")
         
-
     try:
         response = client.responses.create(
             model="gpt-4.1",
@@ -76,12 +79,13 @@ def post_gpt_make_workbook(pdf, difficulty, multiple_choice_amount, true_false_a
 
 # GPT에게 피드백 생성
 def post_gpt_make_feedback(question, choices, answer):
-    request_msg = "한국어로"
-    request_msg += "해당 질문과 답에 맞는 풀이 설명을 해줘"
-    request_msg += "문제는" + question + "이고"
-    request_msg += "선택지는" + choices + "이고"
-    request_msg += "내가 선택한 정답은" + answer + "이다"
-    request_msg += "내가 왜 이 답을 선택했는지, 어떤 개념이 부족한지 문제와 풀이를 설명해달라."
+    request_msg = "한국어로 " + "\n"
+    request_msg += "해당 질문과 답에 맞는 풀이 설명을 해줘 " + "\n"
+    request_msg += "문제는 " + question + "이고 " + "\n"
+    request_msg += "선택지는 " + choices + "이고 " + "\n"
+    request_msg += "내가 선택한 정답은 " + answer + "이다. " + "\n"
+    request_msg += "내가 왜 이 답을 선택했는지, 어떤 개념이 부족한지 문제와 풀이를 설명해달라." + "\n"
+    request_msg += "내가 원하는 응답 형식: " + RESPONSE_FEEDBACK_FORMAT
 
     try:
         response = client.responses.create(
@@ -114,13 +118,14 @@ def post_gpt_make_feedback(question, choices, answer):
 
 # GPT에게 암기법 생성
 def post_gpt_make_memorization(question, choices, answer, method):
-    request_msg = "한국어로"
-    request_msg += "해당 질문과 답에 맞는 풀이 설명을 해줘"
-    request_msg += "문제는" + question + "이고"
-    request_msg += "선택지는" + choices + "이고"
-    request_msg += "내가 선택한 정답은" + answer + "이다"
-    request_msg += "원하는 암기 방법은" + method + "이다"
-    request_msg += "내가 쉽게 외울 수 있는 문장을 만들어줘"
+    request_msg = "한국어로 " + "\n"
+    request_msg += "해당 질문과 답에 맞는 풀이 설명을 해줘 " + "\n"
+    request_msg += "문제는 " + question + "이고 " + "\n"
+    request_msg += "선택지는 " + choices + "이고 " + "\n"
+    request_msg += "내가 선택한 정답은 " + answer + "이다. " + "\n"
+    request_msg += "원하는 암기 방법은 " + method + "이다. " + "\n"
+    request_msg += "내가 쉽게 외울 수 있는 문장을 만들어줘. " + "\n"
+    request_msg += "내가 원하는 응답 형식: " + RESPONSE_MEMORIZATION_FORMAT
 
     try:
         response = client.responses.create(
@@ -149,38 +154,4 @@ def post_gpt_make_memorization(question, choices, answer, method):
     res = {"memorize": response.output_text}
 
     return res
-
-
-# 문제 응답 형식 지정
-RESPONSE_WORKBOOK_FORMAT = """
-    [
-        {
-          "question": "Sample question 1",
-          "type": 0,
-          "choices": {
-            "1": "Option 1",
-            "2": "Option 2",
-            "3": "Option 3",
-            "4": "Option 4"
-          },
-          "answer": "1"
-        },
-        {
-          "question": "Sample question 2",
-          "type": 1,
-          "answer": "O"
-        },
-        {
-          "question": "Sample question 3",
-          "type": 2,
-          "answer": "Dolphin"
-        }
-    ]
-
-        Where:
-        - "type" is an integer:
-          - 0 for Multiple Choice
-          - 1 for True/False
-          - 2 for Short Answer
-"""
 
